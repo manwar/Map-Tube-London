bf788c6c2ab9abae903d4975ffce90ad5fb20ac6
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2014-07-06 03:59:37 +0100

    - Updated Build.PL and Makefile.PL regarding pre-requisite of Map::Tube version - Updated Makedfile.PL about the correct details of license

--- lib/Map/Tube/London.pm
+++ lib/Map/Tube/London.pm
@@ -1,6 +1,6 @@
 package Map::Tube::London;
 
-$Map::Tube::London::VERSION = '0.01';
+$Map::Tube::London::VERSION = '0.02';
 
 use 5.006;
 use Data::Dumper;
@@ -18,7 +18,7 @@ Map::Tube::London - Interface to the London Tube Map.
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =head1 DESCRIPTION
 
