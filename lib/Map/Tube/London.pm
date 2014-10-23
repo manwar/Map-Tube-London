31e9bb19feaf4eaf8229553e56ea083b9a327c1e
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2014-10-23 23:39:07 +0100

    - Made the package File::ShareDir::Install pre-requisite in the Makefile.PL file.

--- lib/Map/Tube/London.pm
+++ lib/Map/Tube/London.pm
@@ -1,6 +1,6 @@
 package Map::Tube::London;
 
-$Map::Tube::London::VERSION = '0.09';
+$Map::Tube::London::VERSION = '0.10';
 
 use 5.006;
 use Data::Dumper;
@@ -19,7 +19,7 @@ Map::Tube::London - Interface to the London Tube Map.
 
 =head1 VERSION
 
-Version 0.09
+Version 0.10
 
 =head1 DESCRIPTION
 
--- t/00-load.t
+++ t/00-load.t
@@ -2,9 +2,7 @@
 use 5.006;
 use strict;
 use warnings FATAL => 'all';
-use Test::More;
-
-plan tests => 1;
+use Test::More tests => 1;
 
 BEGIN {
     use_ok( 'Map::Tube::London' ) || print "Bail out!\n";
