f335c077a5c204d3705f789ab72d3748a12ac9b0
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2015-01-21 21:34:31 +0000

    - Upgraded to use Map::Tube v2.69. - Updated map name in the xml data. - Updated tube map images. - Updated pod document of District and Jubilee Line regarding map height and width. - Updated MANIFEST.SKIP file to exclude maps from the distribution.

--- lib/Map/Tube/London/Line/Bakerloo.pm
+++ lib/Map/Tube/London/Line/Bakerloo.pm
@@ -1,6 +1,6 @@
 package Map::Tube::London::Line::Bakerloo;
 
-$Map::Tube::London::Line::Bakerloo::VERSION   = '0.44';
+$Map::Tube::London::Line::Bakerloo::VERSION   = '0.45';
 $Map::Tube::London::Line::Bakerloo::AUTHORITY = 'cpan:MANWAR';
 
 use 5.006;
@@ -12,7 +12,7 @@ Map::Tube::London::Line::Bakerloo - London Tube Map Bakerloo Line stations.
 
 =head1 VERSION
 
-Version 0.44
+Version 0.45
 
 =head1 DESCRIPTION
 
--- lib/Map/Tube/London/Line/Circle.pm
+++ lib/Map/Tube/London/Line/Circle.pm
@@ -1,6 +1,6 @@
 package Map::Tube::London::Line::Circle;
 
-$Map::Tube::London::Line::Circle::VERSION   = '0.44';
+$Map::Tube::London::Line::Circle::VERSION   = '0.45';
 $Map::Tube::London::Line::Circle::AUTHORITY = 'cpan:MANWAR';
 
 use 5.006;
@@ -12,7 +12,7 @@ Map::Tube::London::Line::Circle - London Tube Map Circle Line stations.
 
 =head1 VERSION
 
-Version 0.44
+Version 0.45
 
 =head1 DESCRIPTION
 
