358dedff0350b35e8fb13ca4f6edb624e3123d23
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2015-01-30 23:18:08 +0000

    - Updated link informations for the station 'Euston' in the xml data. - Updated Northern line tube map.

--- lib/Map/Tube/London/Line/Metropolitan.pm
+++ lib/Map/Tube/London/Line/Metropolitan.pm
@@ -1,6 +1,6 @@
 package Map::Tube::London::Line::Metropolitan;
 
-$Map::Tube::London::Line::Metropolitan::VERSION   = '0.48';
+$Map::Tube::London::Line::Metropolitan::VERSION   = '0.49';
 $Map::Tube::London::Line::Metropolitan::AUTHORITY = 'cpan:MANWAR';
 
 use 5.006;
@@ -12,7 +12,7 @@ Map::Tube::London::Line::Metropolitan - London Tube Map Metropolitan Line statio
 
 =head1 VERSION
 
-Version 0.48
+Version 0.49
 
 =head1 DESCRIPTION
 
