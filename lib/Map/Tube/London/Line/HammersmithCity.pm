0c331942e3f3a79f803cf56aa0e7e2052f565f3c
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2017-03-16 11:06:49 +0000

    - Upgraded to use Test::Map::Tube v0.25.

--- lib/Map/Tube/London/Line/HammersmithCity.pm
+++ lib/Map/Tube/London/Line/HammersmithCity.pm
@@ -1,6 +1,6 @@
 package Map::Tube::London::Line::HammersmithCity;
 
-$Map::Tube::London::Line::HammersmithCity::VERSION   = '0.80';
+$Map::Tube::London::Line::HammersmithCity::VERSION   = '0.81';
 $Map::Tube::London::Line::HammersmithCity::AUTHORITY = 'cpan:MANWAR';
 
 use 5.006;
@@ -12,7 +12,7 @@ Map::Tube::London::Line::HammersmithCity - London Tube Map: Hammersmith & City L
 
 =head1 VERSION
 
-Version 0.80
+Version 0.81
 
 =head1 DESCRIPTION
 
