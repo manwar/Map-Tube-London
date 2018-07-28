66e2e1bf6275b3e32309c2e6b25f11c85efe4231
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2018-07-28 09:50:56 +0100

    - Updated section NOTE for the line "District".

--- lib/Map/Tube/London/Line/Victoria.pm
+++ lib/Map/Tube/London/Line/Victoria.pm
@@ -1,6 +1,6 @@
 package Map::Tube::London::Line::Victoria;
 
-$Map::Tube::London::Line::Victoria::VERSION   = '1.11';
+$Map::Tube::London::Line::Victoria::VERSION   = '1.12';
 $Map::Tube::London::Line::Victoria::AUTHORITY = 'cpan:MANWAR';
 
 use 5.006;
@@ -12,7 +12,7 @@ Map::Tube::London::Line::Victoria - London Tube Map: Victoria Line.
 
 =head1 VERSION
 
-Version 1.11
+Version 1.12
 
 =head1 DESCRIPTION
 
